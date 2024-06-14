36f9b633f905c12b05e4b0cae9ffc14a8cc7b0e9
Ankit Pal <ankitp2104@gmail.com>
2024-06-14 23:42:20 +0530

    main.py

--- audio extract/main.py
+++ audio extract/main.py
@@ -3,7 +3,7 @@ import moviepy.editor
 from tkinter.filedialog import *
 
 vid = askopenfilename()
-video = moviepy.editor.VideoFileClip(vid)
+video = moviepy.editor.VideoFileClip(vid) # you can also give the video path or url rather than vid  
 
 aud = video.audio
 aud.write_audiofile("enjoy.mp3")
